0dfdbedfbd4e6b50056d742d12ceb4556ccc24a6
Randy <baseballrandy3@gmail.com>
2021-09-22 00:09:47 -0400

    added function to return stockstats

--- stock_price_tracker.py
+++ stock_price_tracker.py
@@ -1,13 +1,35 @@
-from bs4.element import AttributeValueWithCharsetSubstitution
 import requests
 from bs4 import BeautifulSoup as bs
 import os
 
-r = requests.get('https://finance.yahoo.com/quote/AAPL?p=AAPL&.tsrc=fin-srch')
+symbol = input('Enter ticker symbol: ')
+r = requests.get('https://finance.yahoo.com/quote/' + symbol + '?p=' + symbol + '&.tsrc=fin-srch')
 
-soup=bs(r.content)
+soup=bs(r.content, features="html5lib")
 
-summary_report = soup.find('div', attrs={'id':'quote-summary'})
+company_name = soup.find('h1', attrs={'class':'D(ib) Fz(18px)'})
 
-last_close = summary_report.find('span', attrs={'class':'Trsdu(0.3s)'})
-print(last_close.get_text())
+stock_heading = soup.find('div', attrs={'data-reactid':'47'})
+stock_close = stock_heading.find('span',attrs={'class':'Trsdu(0.3s)'})
+
+left_side_report = soup.find('table', attrs={'data-reactid':'91'})
+right_side_report = soup.find('table', attrs={'data-reactid':'132'})
+
+table_1 = left_side_report.find_all('span', attrs={'class':'Trsdu(0.3s)'})
+table_2 = right_side_report.find_all('span', attrs={'class':'Trsdu(0.3s)'})
+
+table_1_stats = []
+for stat in table_1:
+     table_1_stats.append(stat.get_text())
+    
+table_2_stats = []
+for stat in table_2:
+    table_2_stats.append(stat.get_text())
+
+all_stats = table_1_stats + table_2_stats
+
+print('Company Name: ', company_name.get_text())
+print('Stock Close Price: ', stock_close.get_text())
+print('Table 1 list: ' , table_1_stats)
+print('Table 2 list: ' , table_2_stats)
+print('All stats: ', all_stats)
