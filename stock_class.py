3e6a33696dffe038c6723f1d8a53a24df14fd1f7
Randy <baseballrandy3@gmail.com>
2021-09-25 00:53:56 -0400

    organized file, and added print output

--- stock_class.py
+++ stock_class.py
@@ -1,14 +1,15 @@
 # This file creates a class for the each stock
 class stock_info:
-    def __init__(self, symbol, close, last_close, market_cap, volume):
-        self.symbol = symbol
+    def __init__(self, close, previous_close, market_cap, volume):
         self.close = close
-        self.last_close = last_close
+        self.previous_close = previous_close
         self.market_cap =  market_cap
         self.volume = volume
-    def get_info(self):
-        print(self.symbol + '\t' +
-              self.close + '\t' +
-              self.last_close + '\t' +
-              self.market_cap + '\t' +
-              self.volume)
+    def get_close(self):
+        return(self.close)
+    def get_previous_close(self):
+        return (self.previous_close) 
+    def get_market_cap(self):
+        return (self.market_cap) 
+    def get_volume(self):
+        return (self.volume)
--- stock_price_tracker.py
+++ stock_price_tracker.py
@@ -1,35 +1,69 @@
 import requests
 from bs4 import BeautifulSoup as bs
-import os
+from stock_class import stock_info
 
-symbol = input('Enter ticker symbol: ')
-r = requests.get('https://finance.yahoo.com/quote/' + symbol + '?p=' + symbol + '&.tsrc=fin-srch')
 
-soup=bs(r.content, features="html5lib")
+def load_webpage(symbol):
+    # uses symbol input to request a yahoo page of ticker symbol
+    r = requests.get('https://finance.yahoo.com/quote/' + symbol + '?p=' + symbol + '&.tsrc=fin-srch')
+    webpage=bs(r.content, features="html5lib")
 
-company_name = soup.find('h1', attrs={'class':'D(ib) Fz(18px)'})
+    # this section will look for the first body element
+    body = webpage.find('body')
+    # will store boolean depending on in the body element has the string under
+    is_there_404 = "Our engineers are working quickly to resolve the issue." in body.get_text()
+    if is_there_404:
+        print('Ticker symbol not found. Please enter a valid ticker symbol.')
+    else:
+        return webpage
 
-stock_heading = soup.find('div', attrs={'data-reactid':'47'})
-stock_close = stock_heading.find('span',attrs={'class':'Trsdu(0.3s)'})
+# function will scrape for the stock's main statistics
+def find_stock_info(webpage):
 
-left_side_report = soup.find('table', attrs={'data-reactid':'91'})
-right_side_report = soup.find('table', attrs={'data-reactid':'132'})
+    # returns the company name element
+    company_name = webpage.find('h1', attrs={'class':'D(ib) Fz(18px)'})
 
-table_1 = left_side_report.find_all('span', attrs={'class':'Trsdu(0.3s)'})
-table_2 = right_side_report.find_all('span', attrs={'class':'Trsdu(0.3s)'})
+    # scrapes the heading of the page and extracts the most recent close of the stock
+    stock_heading = webpage.find('div', attrs={'data-reactid':'47'})
+    stock_close = stock_heading.find('span',attrs={'class':'Trsdu(0.3s)'})
 
-table_1_stats = []
-for stat in table_1:
-     table_1_stats.append(stat.get_text())
+    # Yahoo's stock stats are separated into two tables, so I scraped them as separate entities
+    left_table = webpage.find('table', attrs={'data-reactid':'91'})
+    right_table = webpage.find('table', attrs={'data-reactid':'132'})
+    left_table = left_table.find_all('span', attrs={'class':'Trsdu(0.3s)'})# used to get further into table element
+    right_table = right_table.find_all('span', attrs={'class':'Trsdu(0.3s)'})
+
+    # adds all text from both tables into a list
+    all_stats = []
+    for stat in left_table:
+         all_stats.append(stat.get_text())
+    for stat in right_table:
+        all_stats.append(stat.get_text())
     
-table_2_stats = []
-for stat in table_2:
-    table_2_stats.append(stat.get_text())
+    return company_name.get_text(), stock_close.get_text(), all_stats
 
-all_stats = table_1_stats + table_2_stats
+def main():
+    # asks user for ticker symbol
+    symbol = input('Enter ticker symbol: ').strip().upper()
+    print()
+    webpage = load_webpage(symbol)
+    company_name, close_price, all_stats = find_stock_info(webpage)
 
-print('Company Name: ', company_name.get_text())
-print('Stock Close Price: ', stock_close.get_text())
-print('Table 1 list: ' , table_1_stats)
-print('Table 2 list: ' , table_2_stats)
-print('All stats: ', all_stats)
+    # uses stock_class.py to create a stock class and gets all info and returns it into a variable
+    company = stock_info(close_price, all_stats[0],all_stats[6],all_stats[4])
+    close_price = company.get_close()
+    previous_close = company.get_previous_close()
+    market_cap = company.get_market_cap()
+    volume = company.get_volume()
+
+    # prints the stock's stats
+    print('Company: ' + company_name)
+    print('--------------------------------------')
+    print('Close Price: ' + close_price + '\n' +
+          'Previous Close: ' + previous_close + '\n' +
+          'Market Cap: ' + market_cap + '\n' +
+          'Volume: ' + volume)
+    print()
+
+main()
+
